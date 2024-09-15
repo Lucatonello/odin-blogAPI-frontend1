24bc5e37447de0d5b4f76506f69f7786bb24c7a1
Luca <lucatonello097@gmail.com>
2024-09-15 12:41:25 -0300

    fetch data from backend API and display posts on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:3000/posts')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setPosts(data);
+      }).catch(err => console.error('error fetching data', err));
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h1>{post.title}</h1>
+            <hr />
+            <p>{post.text}</p>
+            <p>Created at: {post.addedat}</p>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
